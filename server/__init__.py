b0c05b6a8f87e87610c921461c8b88dac1c1fbeb
ericvuong1 <ericvuong22222@gmail.com>
2019-09-15 01:24:58 -0400

    Extracts most common shape out of image

--- server/__init__.py
+++ server/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
+from collections import Counter
+
+import cv2
+import numpy as np
+
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
@@ -9,15 +14,23 @@ CORS(app)
 #Regular Flask implementation
 app.route('/')
 
+def most_frequent(List):
+    occurence_count = Counter(List)
+    return occurence_count.most_common(1)[0][0]
+
 @app.route('/shape', methods=['POST'])
 def shape():
-    print('hi')
-    print(request)
     file_storage = request.files['file']
-    print(file_storage.filename)
+    byte_array = file_storage.read()
     # file_storage.save('./assets/lol.png')
-    return 'Yes'
+
+    nparr = np.frombuffer(byte_array, np.uint8)
+    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite('newImage.png',img_np)
+
+    return 'hi'
+
 
 # Run
 if __name__ == '__main__':
-    app.run(use_reloader=True, port=5001)
+    app.run(use_reloader=True, host='0.0.0.0', port=5001)
---
+++ server/test.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+from collections import Counter
+
+def most_frequent(List):
+    occurence_count = Counter(List)
+    return occurence_count.most_common(1)[0][0]
+
+img = cv2.imread("newImage.png", cv2.IMREAD_GRAYSCALE)
+# img = cv2.imread("newImage.png")
+
+t = 200
+
+# gray = cv2.cvtColor(src = img, code = cv2.COLOR_BGR2GRAY)
+# blur = cv2.GaussianBlur(src = gray, 
+#     ksize = (5, 5), 
+#     sigmaX = 0)
+# (t, binary) = cv2.threshold(src = blur,
+#     thresh = t, 
+#     maxval = 255, 
+#     type = cv2.THRESH_BINARY)
+# (_, contours, _) = cv2.findContours(image = binary, 
+#     mode = cv2.RETR_EXTERNAL,
+#     method = cv2.CHAIN_APPROX_SIMPLE)
+_, threshold = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+_, contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+height, width = img.shape 
+img_area = height * width
+
+shapes = []
+for cnt in contours:
+    area = cv2.contourArea(cnt)
+
+    # ignore noise
+    if area < 1000:
+        continue
+    
+    # avoid whole img contour
+    if abs(img_area - area) < 2000:
+        continue
+
+    approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+    cv2.drawContours(img, [approx], 0, (0), 5)
+    x = approx.ravel()[0]
+    y = approx.ravel()[1]
+
+    if len(approx) == 3:
+        shape = 'Triangle'
+    elif len(approx) == 4:
+        shape = 'Rectangle'
+    elif len(approx) == 5:
+        shape = 'Pentagon'
+    elif 6 < len(approx) < 15:
+        shape = 'Ellipse'
+    else:
+        shape = 'Circle'
+    shapes.append(shape)
+print(shapes)
+
+cv2.imshow('hi', img)
+cv2.waitKey()
+
+print(most_frequent(shapes))
+
