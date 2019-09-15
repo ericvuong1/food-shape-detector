33e949b31fa4838ac8cf2497d89e6f1570288dfb
ericvuong1 <ericvuong22222@gmail.com>
2019-09-14 21:39:51 -0400

    test implementation

--- server/__init__.py
+++ server/__init__.py
@@ -1,25 +1,23 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
-import shape_detector 
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
-# Detect food shape
-class Shape(Resource):
-    def post(self):
 
 #Regular Flask implementation
 app.route('/')
 
 @app.route('/shape', methods=['POST'])
 def shape():
-    request_json = request.get_json()
-
-    # return ("{}".format(bmr))
-    return jsonify(bmr)
+    print('hi')
+    print(request)
+    file_storage = request.files['file']
+    print(file_storage.stream)
+    file_storage.save('./assets/lol.png')
+    return 'Yes'
 
 # Run
 if __name__ == '__main__':
-    app.run(use_reloader=True)
+    app.run(use_reloader=True, port=5001)
